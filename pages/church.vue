ea5180c6da5f17f9fc1037db13d7f73f4797a3c2
Liam K <karllauy@gmail.com>
2021-10-23 16:56:07 +0800

    detailed church page

--- pages/church.vue
+++ pages/church.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Nav Bar -->
-    <div class="flex bg-gray-50 h-12 border-2 border-gray-50 shadow-md">
+    <div class="flex bg-gray-50 h-12 border-2 border-gray-50 shadow-md w-full">
       <div
         class="flex justify-center items-center text-gray-800"
         @click="onClickBackButton"
@@ -62,10 +62,65 @@
     </div>
 
     <!-- Main -->
-    <div>
-      <div class="flex flex-shrink-0 justify-center items-center">
-        <!-- <img class="h-14 w-14 rounded-full" :src="church.img_url" alt="" /> -->
+    <div class="flex justify-center items-center w-full">
+      <!-- Church Image -->
+      <div class="flex flex-shrink-0 justify-center items-center mr-2">
+        <img class="h-14 w-14 rounded-full" :src="church.img_url" alt="" />
       </div>
+
+      <div class="flex flex-col">
+        <p class="text-xl font-medium text-indigo-600 truncate">
+          {{ church.basic_info.name }}
+        </p>
+        <p class=""></p>
+      </div>
+    </div>
+
+    <div class="w-full flex">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+        />
+      </svg>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+        />
+      </svg>
     </div>
   </div>
 </template>
@@ -74,9 +129,7 @@
 export default {
   components: {},
   data() {
-    return {
-      //   church,
-    }
+    return {}
   },
   methods: {
     onClickBackButton() {
@@ -85,9 +138,12 @@ export default {
     onClickShareButton() {},
     onClickBookmarkButton() {},
   },
-  mounted() {
-    // console.log(this.$route.params.church)
+  computed: {
+    church() {
+      return this.$store.state.church
+    },
   },
+  mounted() {},
 }
 </script>
 
