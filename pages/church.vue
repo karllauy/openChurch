5414b7db80286d62494e295f4a3940bbdec3b00a
ckngaq <ckngaq@connect.ust.hk>
2021-10-24 15:14:47 +0800

    Improve details upper part

--- pages/church.vue
+++ pages/church.vue
@@ -26,28 +26,11 @@
 
       <!-- Justify right -->
       <div
-        class="mr-2 flex justify-center items-center text-gray-800"
+        class="flex justify-center items-center text-gray-800"
       >
-      <!-- Share button -->
-        <div
-          class="flex justify-center items-center text-gray-800"
-          @click="onClickShareButton"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-8 w-8"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"
-            />
-          </svg>
-        </div>
-
         <!-- Bookmark button -->
         <div
-          class="flex justify-center items-center text-gray-800"
+          class="mr-2 flex justify-center items-center text-gray-800"
           @click="onClickBookmarkButton"
         >
           <svg
@@ -65,17 +48,34 @@
             />
           </svg>
         </div>
+
+        <!-- Share button -->
+        <div
+          class="mr-2 flex justify-center items-center text-gray-800"
+          @click="onClickShareButton"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"
+            />
+          </svg>
+        </div>
       </div>
     </div>
 
     <!-- Main -->
-    <div class="flex justify-center items-center w-full">
+    <div class="m-4 flex justify-center items-center w-full">
       <!-- Church Image -->
-      <div class="flex flex-shrink-0 justify-center items-center mr-2">
-        <img class="h-14 w-14 rounded-full" :src="church.img_url" alt="" />
+      <div class="w-1/3 flex flex-shrink-0 justify-center items-center mr-2">
+        <img class="h-24 w-24 rounded-full" :src="church.img_url" alt="" />
       </div>
 
-      <div class="flex flex-col">
+      <div class="w-2/3 flex flex-col">
         <p class="text-xl font-medium text-indigo-600 truncate">
           {{ church.basic_info.name }}
         </p>
@@ -83,6 +83,7 @@
       </div>
     </div>
 
+    <!-- Contact -->
     <div class="w-full justify-center items-center flex">
       <!-- WhatsApp -->
       <div class="w-1/5 justify-center items-center flex">
