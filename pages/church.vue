26cd47f166ce72009d224d26bf5e9380dd887705
ckngaq <ckngaq@connect.ust.hk>
2021-10-24 15:02:17 +0800

    Improve upper part of details

--- pages/church.vue
+++ pages/church.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <!-- Nav Bar -->
-    <div class="flex bg-gray-50 h-12 border-2 border-gray-50 shadow-md w-full">
+    <div class="flex justify-between bg-gray-50 h-12 border-2 border-gray-50 shadow-md w-full">
+
+      <!-- Justify left -->
       <div
-        class="flex justify-center items-center text-gray-800"
+        class="ml-2 flex justify-center items-center text-gray-800"
         @click="onClickBackButton"
       >
         <svg
@@ -22,42 +24,47 @@
         </svg>
       </div>
 
+      <!-- Justify right -->
+      <div
+        class="mr-2 flex justify-center items-center text-gray-800"
+      >
       <!-- Share button -->
-      <div
-        class="flex justify-center items-center text-gray-800"
-        @click="onClickShareButton"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-8 w-8"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+        <div
+          class="flex justify-center items-center text-gray-800"
+          @click="onClickShareButton"
         >
-          <path
-            d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"
-          />
-        </svg>
-      </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"
+            />
+          </svg>
+        </div>
 
-      <!-- Bookmark button -->
-      <div
-        class="flex justify-center items-center text-gray-800"
-        @click="onClickBookmarkButton"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-8 w-8"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+        <!-- Bookmark button -->
+        <div
+          class="flex justify-center items-center text-gray-800"
+          @click="onClickBookmarkButton"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
-          />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+            />
+          </svg>
+        </div>
       </div>
     </div>
 
